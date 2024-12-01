6f712bf462c4b69ef30fa929bfc5cecae7df4dee
Connor Spear <cspeardev@gmail.com>
2024-12-01 15:25:29 -0500

    🚀 Rework part one loop

--- 2024/day_1.py
+++ 2024/day_1.py
@@ -1,13 +1,10 @@
 #!/usr/bin/env python3
 '''Day one of Advent of Code'''
+import collections
 import os
 import time
 
 start_time = time.time()
-
-def get_index_of_lowest(array:list[int])-> int:
-  lowest = min(array)
-  return array.index(lowest)
 
 script_dir = os.path.dirname(__file__)
 input_file_path = script_dir + '/input/1'
@@ -20,17 +17,10 @@ with open(input_file_path,encoding='utf-8') as input_file:
   first_array = [int(line[0:5]) for line in lines]
   second_array = [int(line[8:]) for line in lines]
 
-pairs = [()]*0
-first_array_part_1 = first_array.copy()
-second_array_part_1 = second_array.copy()
+first_array.sort()
+second_array.sort()
 
-while len(first_array_part_1) > 0:
-  first_lowest_index = first_array_part_1.index(min(first_array_part_1))
-  second_lowest_index = second_array_part_1.index(min(second_array_part_1))
-  pairs.append((first_array_part_1[first_lowest_index],
-                second_array_part_1[second_lowest_index]))
-  del first_array_part_1[first_lowest_index]
-  del second_array_part_1[second_lowest_index]
+pairs = zip(first_array,second_array)
 
 diffs = (abs(pair[0] - pair[1]) for pair in pairs)
 
