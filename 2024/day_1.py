8b0c8e243ad70aa5e2dfbbb89cba1f63593c599a
Connor Spear <cspeardev@gmail.com>
2024-12-01 14:50:54 -0500

    ✨ Solution for part 2 of day one

--- 2024/day_1.py
+++ 2024/day_1.py
@@ -30,14 +30,16 @@ with open(input_file_path,encoding='utf-8') as input_file:
     second_array.append(second)
 
 pairs = [()]*0
+first_array_part_1 = first_array.copy()
+second_array_part_1 = second_array.copy()
 
-while len(first_array) > 0:
-  first_lowest_index = get_index_of_lowest(first_array)
-  second_lowest_index = get_index_of_lowest(second_array)
-  pairs.append((first_array[first_lowest_index],
-                second_array[second_lowest_index]))
-  del first_array[first_lowest_index]
-  del second_array[second_lowest_index]
+while len(first_array_part_1) > 0:
+  first_lowest_index = get_index_of_lowest(first_array_part_1)
+  second_lowest_index = get_index_of_lowest(second_array_part_1)
+  pairs.append((first_array_part_1[first_lowest_index],
+                second_array_part_1[second_lowest_index]))
+  del first_array_part_1[first_lowest_index]
+  del second_array_part_1[second_lowest_index]
 
 diffs = []
 
@@ -46,9 +48,27 @@ for pair in pairs:
 
 sum_of_diffs = sum(diffs)
 
-print('Solution is ' + str(sum_of_diffs))
+print('Solution for part one is ' + str(sum_of_diffs))
 
 end_time = time.time()
 elapsed_time = end_time - start_time
 elapsed_time_in_milliseconds = elapsed_time * 1000
-print('Elapsed time: ' + str(elapsed_time_in_milliseconds))
+print('Elapsed time for part one: ' + str(elapsed_time_in_milliseconds))
+
+# --- Part Two
+start_time = time.time()
+multiplied_numbers = []
+
+for number in first_array:
+  occurences = second_array.count(number)
+  multiplied_numbers.append(number*occurences)
+
+similarity_score = sum(multiplied_numbers)
+
+print('Solution for part two is ' + str(similarity_score))
+
+end_time = time.time()
+elapsed_time = end_time - start_time
+elapsed_time_in_milliseconds = elapsed_time * 1000
+print('Elapsed time for part two: ' + str(elapsed_time_in_milliseconds))
+
