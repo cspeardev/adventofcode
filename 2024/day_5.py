5867a4c7f57ceaa3a74c7de9997fe4487e0d2bd3
Connor Spear <cspeardev@gmail.com>
2024-12-05 23:42:06 -0500

    ✨ Solution for Day five, part two

--- 2024/day_5.py
+++ 2024/day_5.py
@@ -3,6 +3,9 @@
 import time
 import os
 
+def sum_middle_values(sets):
+  return sum([page_set[int(len(page_set)/2)] for page_set in sets])
+
 # --- Part One
 
 start_time = time.time()
@@ -26,30 +29,68 @@ pages_sets=[list(int(value) for value in line.split(','))
 
 valid_page_sets=[]
 
+# Assume that any pages only occur once in each set
+
 for pages in pages_sets:
   is_page_set_valid = True
   applicable_rules_for_page_set = []
   for rule in rules:
     if all(element in pages for element in rule):
       applicable_rules_for_page_set.append(rule)
-  for i,page in enumerate(pages):
+  for page_index,page in enumerate(pages):
     applicable_rules_for_page = [rule for rule
                                  in applicable_rules_for_page_set
                                  if rule[0] == page]
     for rule in applicable_rules_for_page:
-      occurence_indexes = [index for index,value
-                           in enumerate(pages) if value == rule[1]]
-      if any(index < i for index in occurence_indexes):
+      occurence_index = pages.index(rule[1])
+      if occurence_index < page_index:
         is_page_set_valid = False
         break
+
   if is_page_set_valid:
     valid_page_sets.append(pages)
 
-solution = sum([page_set[int(len(page_set)/2)] for page_set in valid_page_sets])
+
+
+solution = sum_middle_values(valid_page_sets)
 
 print(f"Solution for part 1 is {solution}")
 
 end_time = time.time()
 elapsed_time = end_time - start_time
 elapsed_time_in_milliseconds = elapsed_time * 1000
-print('Elapsed time for part one: ' + str(elapsed_time_in_milliseconds))
+print('Elapsed time for part 2: ' + str(elapsed_time_in_milliseconds))
+
+# --- Part 2
+start_time = time.time()
+corrected_page_sets = []
+
+for pages in pages_sets:
+  add_this_set = False
+  is_page_set_valid = True
+  applicable_rules_for_page_set = []
+  for rule in rules:
+    if all(element in pages for element in rule):
+      applicable_rules_for_page_set.append(rule)
+  for page_index,page in enumerate(pages):
+    applicable_rules_for_page = [rule for rule
+                                 in applicable_rules_for_page_set
+                                 if rule[0] == page]
+    for before,after in applicable_rules_for_page:
+      after_index = pages.index(after)
+      before_index = pages.index(before)
+      if after_index < before_index:
+        add_this_set = True
+        value = pages.pop(before_index)
+        pages.insert(after_index,value)
+  if add_this_set:
+    corrected_page_sets.append(pages)
+
+
+solution = sum_middle_values(corrected_page_sets)
+print(f"Solution for part 2 is {solution}")
+
+end_time = time.time()
+elapsed_time = end_time - start_time
+elapsed_time_in_milliseconds = elapsed_time * 1000
+print('Elapsed time for part 2: ' + str(elapsed_time_in_milliseconds))
