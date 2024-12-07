04d940cd94e25719f6e1c8d469e45385b416c251
Connor Spear <cspeardev@gmail.com>
2024-12-07 15:11:31 -0500

    🚧 Day 6 improvements

--- 2024/day_6.py
+++ 2024/day_6.py
@@ -1,51 +1,52 @@
 #!/usr/bin/env python3
 '''Day six of Advent of Code'''
+from collections import deque
 import time
 import os
 
 def walk_obstacles(width, height, start, obstacles):
-  visited_positions=[]*0
-  directions=[(-1,0),(0,1),(1,0),(0,-1)]
+  visited_positions=set()
+  directions=deque([(-1,0),(0,1),(1,0),(0,-1)])
   current_direction = directions[0]
   current_position = start
-  while(0 <= current_position[0] <= width
-    and 0 <= current_position [1] <= height):
+  next_position=current_position
+  while(0 <= next_position[0] <= width
+    and 0 <= next_position[1] <= height):
     if current_position not in visited_positions:
-      visited_positions.append(current_position)
-    next_position = (current_position[0]+current_direction[0],
-                  current_position[1]+current_direction[1])
+      visited_positions.add(current_position)
     if next_position in obstacles:
-      current_direction_index = directions.index(current_direction)
-      next_direction_index = (current_direction_index+1) % len(directions)
-      current_direction=directions[next_direction_index]
+      directions.rotate(-1)
+      current_direction=directions[0]
       next_position = (current_position[0]+current_direction[0],
                   current_position[1]+current_direction[1])
     current_position = next_position
+    next_position = (current_position[0]+current_direction[0],
+                  current_position[1]+current_direction[1])
   return visited_positions
 
-def walk_obstacles_find_loop(width, height, start, obstacles):
+def walk_obstacles_find_loop(width:int,
+                             height:int,
+                             start:tuple[int,int],
+                             obstacles:set[tuple[int,int]]):
   visited_positions=set()
-  directions=[(-1,0),(0,1),(1,0),(0,-1)]
+  directions=deque([(-1,0),(0,1),(1,0),(0,-1)])
   current_direction = directions[0]
   current_position = start
   while(0 <= current_position[1] <= width
     and 0 <= current_position [0] <= height):
-    if current_position not in visited_positions:
-      visited_positions.add((current_position,current_direction))
     next_position = (current_position[0]+current_direction[0],
                   current_position[1]+current_direction[1])
     if next_position in obstacles:
-      current_direction_index = directions.index(current_direction)
-      next_direction_index = (current_direction_index+1) % len(directions)
-      current_direction=directions[next_direction_index]
+      directions.rotate(-1)
+      current_direction=directions[0]
       next_position = (current_position[0]+current_direction[0],
                   current_position[1]+current_direction[1])
     current_position = next_position
-    if (current_position,current_direction) in visited_positions:
+    if (next_position,current_direction) in visited_positions:
       return True
+    if current_position not in visited_positions:
+      visited_positions.add((current_position,current_direction))
   return False
-
-
 
 def main():
   script_dir = os.path.dirname(__file__)
@@ -60,41 +61,34 @@ def main():
   print(f'Solution for part 1: {part_1_solution}')
 
   part_1_elapsed_time = part_1_end_time - part_1_start_time
-  elapsed_time_in_milliseconds = part_1_elapsed_time * 1000
-  print(f'Elapsed time for part 1: {str(elapsed_time_in_milliseconds*1000)} ms')
+  print(f'Elapsed time for part 1: {str(part_1_elapsed_time*1000)} ms')
 
-  part_2_start_time = time.time()
-  part_2_solution = part_2(lines)
-  part_2_end_time = time.time()
-  print(f'Solution for part 2: {part_2_solution}')
+  # part_2_start_time = time.time()
+  # part_2_solution = part_2(lines)
+  # part_2_end_time = time.time()
+  # print(f'Solution for part 2: {part_2_solution}')
 
-  part_2_elapsed_time = part_2_end_time - part_2_start_time
-  elapsed_time_in_milliseconds = part_2_elapsed_time * 1000
-  print(f'Elapsed time for part 2: {str(elapsed_time_in_milliseconds*1000)} ms')
+  # part_2_elapsed_time = part_2_end_time - part_2_start_time
+  # print(f'Elapsed time for part 2: {str(part_2_elapsed_time*1000)} ms')
 
 
 def part_1(lines):
   starting_position = (0,0)
-
-  for i, line in enumerate(lines):
-    if '^' in line:
-      starting_position = (i,line.index('^'))
-
+  starting_position = find_starting_position(lines)
   obstacle_coordinates= find_obstacles(lines)
 
   lines_width=len(lines[0])
   lines_height=len(lines)
-  solution=len(walk_obstacles(lines_width, lines_height , starting_position, obstacle_coordinates))-1
+  solution=len(walk_obstacles(lines_width,
+                              lines_height,
+                              starting_position,
+                              obstacle_coordinates))
   return solution
 
 
 def part_2(lines):
   solution=0
-  starting_position = (0,0)
-  for i, line in enumerate(lines):
-    if '^' in line:
-      starting_position = (i,line.index('^'))
-
+  starting_position = find_starting_position(lines)
   obstacle_coordinates = find_obstacles(lines)
 
   lines_width=len(lines[0])
@@ -110,6 +104,11 @@ def part_2(lines):
 
   return solution
 
+def find_starting_position(lines):
+  for i, line in enumerate(lines):
+    if '^' in line:
+      return (i,line.index('^'))
+
 def find_obstacles(lines):
   obstacle_coordinates=set()
   for i, line in enumerate(lines):
@@ -121,3 +120,6 @@ def find_obstacles(lines):
 
 if __name__ == '__main__':
   main()
+
+
+#1650 is wrong
